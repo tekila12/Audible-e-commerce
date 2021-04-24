168f79fce8e33868828465e94048ee871ca9dd53
Ajnur Radovic <radovic.ajnur96@gmail.com>
2021-04-24 21:10:31 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.static("."));
 app.use(errorHandler);
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,'/client/build')))
+  app.use(express.static(path.join(__dirname,'/client/build')));
   app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html' ))
   })
