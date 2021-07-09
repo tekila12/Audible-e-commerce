f6dbf40716d5a0f7b02e826f55bf11a9cf149199
Ajnur Radovic <72083386+tekila12@users.noreply.github.com>
2021-07-09 17:37:03 +0200

    Update user.js

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useHistory } from "react-router-dom"
 const UserContext = React.createContext();
 
+/*Saving authToken in the local storage so the user keeps being
+logged in when the page is refreshed */
+
 function getUserFromLocalStorage() {
     return localStorage.getItem("user")
       ? JSON.parse(localStorage.getItem("user"))
@@ -13,6 +16,11 @@ function UserProvider({ children }) {
   const [user, setUser] = React.useState(getUserFromLocalStorage());
   const history = useHistory()
 
+  /* Logout funcionality 
+  
+  So here I remove the previousUser, and I am setting token to null || 
+  I am cleaning it from localStorage where it was kept. */
+  
   const logoutHandler = React.useCallback(() => {
     localStorage.removeItem('user')
     setUser((previousUser) => ({ ...previousUser, token: null }))
@@ -20,6 +28,10 @@ function UserProvider({ children }) {
     history.push('/')
   }, [history])
 
+  
+  /* Here useMemo is being used because everytime a Component uses any of these functions
+  a new object is being created and I dont wan't that */
+  
    const myUserContextValue =React.useMemo(()=>({
       user, setUser, logoutHandler, success, setSuccees
     }),[user, setUser, logoutHandler, success, ])
